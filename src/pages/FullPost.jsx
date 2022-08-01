33e052bcaaee11efe8959ab70045e10acda3c320
tolganaya <tolganay.akylbekova@gmail.com>
2022-08-01 22:55:36 +0600

    workbtch

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -33,9 +33,8 @@ export const FullPost = () => {
     <>
       <Post
         id={data._id}
-        title={data.title}
+        title={data.title} 
         imageUrl={data.imageUrl ? `${process.env.REACT_APP_API_URL}${data.imageUrl}` : ''}
-        // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
