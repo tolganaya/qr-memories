f6f47cb75253450d9994d76140437353d1a15801
Tolganay <96404584+tolganaya@users.noreply.github.com>
2022-08-02 17:04:06 +0600

    Update QrPost.jsx

--- src/components/QR/QrPost.jsx
+++ src/components/QR/QrPost.jsx
@@ -54,7 +54,7 @@ export const QrPost = () => {
 
   useEffect(() => {
     qrCode.update({
-      data: `${process.env.API_URL}/posts/${id}`
+      data: `${process.env.REACT_APP_WEB_URL}/posts/${id}`
     });
   }, [id]);
 
@@ -129,7 +129,7 @@ const styles = {
     
 //       <QRCode 
 //       className=""
-//       value={`${process.env.API_URL}/posts/${id}`} />
+//       value={`${process.env.REACT_APP_WEB_URL}/posts/${id}`} />
 //     </>
 //   );
 // };
